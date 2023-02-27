ac31799b399399eca60b0c6a5bd9efc856bd71ff
tiredsosha <tiredsosha@gmail.com>
2023-02-27 15:28:53 +0000

    changed frontend web port

--- src/index.jsx
+++ src/index.jsx
@@ -6,8 +6,8 @@ import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import App from './App';
 
-axios.defaults.baseURL = 'http://localhost:80';
-axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.withCredentials = false;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
