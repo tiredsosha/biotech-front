6d4d6c510657689e8c26095305b3e5986f633c6a
Povyshka <povyshka@gmail.com>
2023-03-01 18:07:22 +0300

    styles change

--- src/OneZone.jsx
+++ src/OneZone.jsx
@@ -6,13 +6,13 @@ export default function OneZone({ oneZone }) {
 
   return (
 
-    <div className="main_zones__content" style={oneZone.status !== 'OK' ? { backgroundColor: '#ffaeae' } : { backgroundColor: 'white' }}>
+    <div className="main_zones__content" style={oneZone.status !== 'OK' ? { borderColor: 'red' } : { borderColor: '' }}>
       <div className="main_zones__content__click" onClick={() => setModal(true)}>
         <div className="main_zones__content__image">
           <img src="img/biotech-img.png" alt="" className="img" />
         </div>
         <div className="main_zones__content__text">
-          <h3>{oneZone.rusName}</h3>
+          <h3 style={oneZone.status !== 'OK' ? { color: 'red' } : { color: '' }}>{oneZone.rusName}</h3>
         </div>
       </div>
 
