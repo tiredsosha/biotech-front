b7526c01d0ceddc1515671080ff86464cd9868ba
tiredsosha <tiredsosha@gmail.com>
2023-04-11 19:25:40 +0100

    kk

---
+++ src/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
+import App from './App';
+
+axios.defaults.baseURL = 'http://192.168.10.200:8080';
+axios.defaults.withCredentials = false;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
+);
